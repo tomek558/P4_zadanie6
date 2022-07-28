83a9582f3184e1ef76722740b96347338d01e424
tomek558 <tk055569@student.ath.edu.pl>
2022-07-28 12:27:26 +0200

    grrrr

---
+++ zadanie6/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace zadanie6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var path = Environment.CurrentDirectory + @"\ModifiedDictionary.xaml";
+            var stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            var content = XamlReader.Load(stream);
+            var dictionary = (ResourceDictionary) content;
+            Resources.MergedDictionaries.Add(dictionary);
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            var path = Environment.CurrentDirectory + @"\DefaultDictionary.xaml";
+            var stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            var content = XamlReader.Load(stream);
+            var dictionary = (ResourceDictionary)content;
+            Resources.MergedDictionaries.Add(dictionary);
+        }
+    }
+}
